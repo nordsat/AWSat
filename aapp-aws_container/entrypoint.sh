0e8f78dd3fb2aee948e3c4d149b20a33bc9a8687
Panu Lahtinen <panu.lahtinen@fmi.fi>
2025-09-19 09:54:30 +0300

    Fix L1C BUFR naming

--- aapp-aws_container/entrypoint.sh
+++ aapp-aws_container/entrypoint.sh
@@ -8,5 +8,7 @@ source /opt/bin/aapp_aws_env.sh
 
 cd $L1_INPUT_DIR
 for f in *nc; do
-    /opt/bin/aws_netcdf_to_bufr_1c.exe -i $f -b 3 -n /config/aws_bufr.nl -a /config/aws_averaging.nl -o $L1C_OUTPUT_DIR/${f%.nc}.bufr
+    out_fname=${f%.nc}.bufr
+    out_fname=`echo $out_fname | sed -e "s/1B/1C/g"`
+    /opt/bin/aws_netcdf_to_bufr_1c.exe -i $f -b 3 -n /config/aws_bufr.nl -a /config/aws_averaging.nl -o $L1C_OUTPUT_DIR/${out_fname}
 done
